f4626bee38f6d13ba343b33d8fe1d2638f96b142
Berkty Tassaw <56670510+brkty-co@users.noreply.github.com>
2023-09-07 10:53:34 +0300

    Adding project files

---
+++ Fiermata.com/Fiermata.com/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fiermata.com.Models;
+using Fiermata.com.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Fiermata.com.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileBusService BusService;
+        public IEnumerable<Bus> Buses { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileBusService Busservice)
+        {
+            _logger = logger;
+            BusService = Busservice;
+        }
+
+        public void OnGet()
+        {
+            Buses = BusService.GetBuses();
+        }
+    }
+}
---
+++ Fiermata.com/Fiermata.com/Services/JsonFileBusService.cs
@@ -0,0 +1,40 @@
+﻿using Fiermata.com.Models;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Fiermata.com.Services
+{
+    public class JsonFileBusService
+    {
+
+        public JsonFileBusService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "Data", "BusList.json"); }
+        }
+
+        public IEnumerable<Bus> GetBuses()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Bus[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
+
